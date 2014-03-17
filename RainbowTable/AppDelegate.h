25112d08fba52990af83d5c33958412ce8e6da54
Aaron Dufall <aald212@gmail.com>
2014-03-17 08:30:54 -0700

    Initial Commit

---
+++ RainbowTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RainbowTable
+//
+//  Created by Aaron Dufall on 17/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RainbowTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RainbowTable
+//
+//  Created by Aaron Dufall on 17/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
